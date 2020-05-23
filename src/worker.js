94c8a45197c80992a59bcd316029d0d6c216abe8
Nithil N <nithiln96@gmail.com>
2020-05-23 11:06:13 +0530

    Create individual channel for each worker queue

--- src/worker.js
+++ src/worker.js
@@ -12,18 +12,24 @@ const { sleep } = require('./helpers/sleep');
 
 const listenToHelloQueue = async (broker) => {
   try {
+    const channel = await broker.connection.createChannel();
+    // This tells RabbitMQ not to give more than two message to a worker at a time (Fair dispatch)
+    channel.prefetch(2);
+
+    // asserts the queue exists, if it doesn’t exist assertQueue will create the queue
     const helloQueue = 'hello';
+    await channel.assertQueue(helloQueue, { durable: true });
 
     // worker 1 for helloQueue
     console.log(' [*] worker 1 waiting for messages in %s ', helloQueue);
-    broker.channel.consume(
+    channel.consume(
       helloQueue,
       async (msg) => {
         await sleep(5000);
         console.log(' [x] Received by worker - 1 %s', msg.content.toString());
 
         // An ack(nowledgement) is sent back by the consumer to tell RabbitMQ that a particular message has been received, processed and that RabbitMQ is free to delete it.
-        broker.channel.ack(msg);
+        channel.ack(msg);
       },
       // when noAck is true the rabbitmq server won’t wait for the acknowledgment from the client side after processing. The server will delete the message from the queue as soon as it is delivered to the client.
       { noAck: false }
@@ -31,7 +37,7 @@ const listenToHelloQueue = async (broker) => {
 
     // worker 2 for helloQueue
     console.log(' [*] worker 2 waiting for messages in %s ', helloQueue);
-    broker.channel.consume(
+    channel.consume(
       helloQueue,
       async (msg) => {
         console.log(' [x] Received by worker - 2 %s', msg.content.toString());
@@ -45,32 +51,40 @@ const listenToHelloQueue = async (broker) => {
 
 const listenToEmailQueue = async (broker) => {
   try {
+    const channel = await broker.connection.createChannel();
+    // This tells RabbitMQ not to give more than four message to a worker at a time (Fair dispatch)
+    channel.prefetch(4);
+
     const emailQueue = 'email';
+    await channel.assertQueue(emailQueue, { durable: true });
+
     // worker 1 for emailQueue
     console.log(' [*] worker 1 waiting for messages in %s ', emailQueue);
-    broker.channel.consume(
+    channel.consume(
       emailQueue,
       async (msg) => {
         console.log(' [x] Received by worker - 1 %s', msg.content.toString());
 
         const form = JSON.parse(msg.content.toString());
         await sendEmailTo(form.email);
-        broker.channel.ack(msg);
+
+        channel.ack(msg);
       },
       { noAck: false }
     );
 
     // worker 2 for emailQueue
     console.log(' [*] worker 2 waiting for messages in %s ', emailQueue);
-    broker.channel.consume(
+    channel.consume(
       emailQueue,
       async (msg) => {
-        // await sleep(5000);
+        await sleep(5000);
         console.log(' [x] Received by worker - 2 %s', msg.content.toString());
 
         const form = JSON.parse(msg.content.toString());
         await sendEmailTo(form.email);
-        broker.channel.ack(msg);
+
+        channel.ack(msg);
       },
       { noAck: false }
     );
@@ -82,10 +96,6 @@ const listenToEmailQueue = async (broker) => {
 const connectAndListen = async () => {
   try {
     const broker = await MessageBroker.getInstance();
-
-    // This tells RabbitMQ not to give more than two message to a worker at a time (Fair dispatch)
-    broker.channel.prefetch(2);
-    // TODO: set prefetch for each queue differently
 
     await listenToHelloQueue(broker);
     await listenToEmailQueue(broker);
